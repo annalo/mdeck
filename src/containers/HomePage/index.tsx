92b5deaf7f17b4f9fee01052ab016b461007390d
Anna Lo <annayylo@gmail.com>
2020-08-13 01:43:17 -0400

    move out parsing logic to parse.tsx util

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -2,25 +2,15 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import styled from "styled-components/macro";
 
-import DOMPurify from "dompurify";
-import MarkdownIt from "markdown-it";
-import MarkdownItTaskLists from "markdown-it-task-lists";
-
 import { TextEditor } from "containers/TextEditor/Loadable";
+import { parse } from "utils/parse";
 
-export const HomePage = () => {
+const Div = styled.div`
+  height: 100%;
+`;
+
+export function HomePage() {
   const [markdown, setMarkdown] = useState("");
-
-  function sanitizeHTML(dirty: string): string {
-    return DOMPurify.sanitize(dirty);
-  }
-
-  function parseMarkdown(text: string): string {
-    const parser = new MarkdownIt().use(MarkdownItTaskLists);
-    const result = parser.render(text);
-
-    return sanitizeHTML(result);
-  }
 
   return (
     <>
@@ -35,16 +25,10 @@ export const HomePage = () => {
         <TextEditor markdown={markdown} setMarkdown={setMarkdown} />
         <div
           dangerouslySetInnerHTML={{
-            __html: parseMarkdown(markdown),
+            __html: parse(markdown),
           }}
         />
       </Div>
     </>
   );
-};
-
-const Div = styled.div`
-  height: 100%;
-`;
-
-export default HomePage;
+}
---
+++ src/utils/parse.tsx
@@ -0,0 +1,14 @@
+import DOMPurify from "dompurify";
+import MarkdownIt from "markdown-it";
+import MarkdownItTaskLists from "markdown-it-task-lists";
+
+function sanitizeHTML(dirty: string): string {
+  return DOMPurify.sanitize(dirty);
+}
+
+export function parse(text: string): string {
+  const parser = new MarkdownIt().use(MarkdownItTaskLists);
+  const result = parser.render(text);
+
+  return sanitizeHTML(result);
+}
