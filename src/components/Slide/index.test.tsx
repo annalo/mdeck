bf50060ab25b9587e4ae29d3898fd63c0a338bb7
Anna Lo <annayylo@gmail.com>
2020-08-13 23:46:47 -0400

    add Slide component

---
+++ src/components/Slide/index.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { Slide } from ".";
+
+const renderComponent = () => render(<Slide />);
+
+describe("<Slide />", () => {
+  it("should match the snapshot", () => {
+    const component = renderComponent();
+    expect(component.container.firstChild).toMatchSnapshot();
+  });
+});
