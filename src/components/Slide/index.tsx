b0761c351debc9404799660b8a025f0670cb7b2a
Anna Lo <annayylo@gmail.com>
2020-08-13 23:24:05 -0400

    remove slide components

--- src/components/Slide/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { memo } from "react";
-import styled from "styled-components/macro";
-
-const Container = styled.div``;
-
-export const Slide: React.FC = memo(() => {
-  return <Container />;
-});
