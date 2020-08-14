22ebaabbfe88c06361c5a09a99281b9b7f0fb577
Anna Lo <annayylo@gmail.com>
2020-08-14 03:22:25 -0400

    remove all paddings and margins on slideshow and slide component

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -11,7 +11,6 @@ const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-  padding: 10px;
 `;
 
 export const Preview: React.FC<Props> = memo(({ slides }: Props) => (
--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -15,14 +15,14 @@ interface Props {
 
 const Container = styled.div`
   height: 0;
-  margin: 10px;
   padding-top: calc((9 / 16) * 100%);
   position: relative;
+  overflow: hidden;
 
   background-color: white;
-  -webkit-box-shadow: 0px 0px 6px 1px rgba(207, 205, 207, 0.5);
-  -moz-box-shadow: 0px 0px 6px 1px rgba(207, 205, 207, 0.5);
-  box-shadow: 0px 0px 6px 1px rgba(207, 205, 207, 0.5);
+  -webkit-box-shadow: 0px 0px 5px 1px rgba(207, 205, 207, 0.5);
+  -moz-box-shadow: 0px 0px 5px 1px rgba(207, 205, 207, 0.5);
+  box-shadow: 0px 0px 5px 1px rgba(207, 205, 207, 0.5);
 
   > :first-child {
     position: absolute;
