4eb0248726b26f1fe19fcebcfdbb97186c7dad98
Anna Lo <annayylo@gmail.com>
2020-08-18 19:30:37 -0400

    extract injectLineNumber out to own file

--- src/utils/parse.tsx
+++ src/utils/parse.tsx
@@ -1,54 +1,6 @@
-/* eslint-disable */
 import { Marpit } from "@marp-team/marpit";
 import TaskList from "markdown-it-task-lists";
-
-const RULES = [
-  "blockquote_open",
-  "code_block",
-  "fence",
-  "heading_open",
-  "image",
-  "link_open",
-  "list_item_open",
-  "paragraph_open",
-  "table_open",
-];
-
-function injectLineNumber(md: any) {
-  const { marpit_inline_svg_open } = md.renderer.rules;
-
-  // Enable line sync by per slides
-  md.renderer.rules.marpit_inline_svg_open = (tokens, idx, opts, env, self) => {
-    const slide = tokens
-      .slice(idx + 1)
-      .find((t) => t.type === "marpit_slide_open");
-
-    if (slide.map?.length) {
-      tokens[idx].attrJoin("class", "src-line");
-      tokens[idx].attrSet("data-line", slide.map[0]);
-    }
-
-    const renderer = marpit_inline_svg_open || self.renderToken;
-    return renderer.call(self, tokens, idx, opts, env, self);
-  };
-
-  // Enables line sync per elements
-  for (const rule of RULES) {
-    const original = md.renderer.rules[rule];
-
-    md.renderer.rules[rule] = (tokens, idx, options, env, self) => {
-      const token = tokens[idx];
-
-      if (token.map?.length) {
-        token.attrJoin("class", "code-line");
-        token.attrSet("data-line", token.map[0]);
-      }
-
-      const renderer = original || self.renderToken;
-      return renderer.call(self, tokens, idx, options, env, self);
-    };
-  }
-}
+import { injectLineNumber } from "utils/parsePlugins/injectLineNumber";
 
 export function parse(markdown: string): string {
   const marpit = new Marpit({
---
+++ src/utils/parsePlugins/injectLineNumber.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/naming-convention */
+
+const RULES = [
+  "blockquote_open",
+  "code_block",
+  "fence",
+  "heading_open",
+  "image",
+  "link_open",
+  "list_item_open",
+  "paragraph_open",
+  "table_open",
+];
+
+export function injectLineNumber(md: any) {
+  const { marpit_inline_svg_open } = md.renderer.rules;
+
+  // Enable line sync by per slides
+  md.renderer.rules.marpit_inline_svg_open = (tokens, idx, opts, env, self) => {
+    const slide = tokens
+      .slice(idx + 1)
+      .find((t) => t.type === "marpit_slide_open");
+
+    if (slide.map?.length) {
+      tokens[idx].attrJoin("class", "src-line");
+      tokens[idx].attrSet("data-line", slide.map[0]);
+    }
+
+    const renderer = marpit_inline_svg_open || self.renderToken;
+    return renderer.call(self, tokens, idx, opts, env, self);
+  };
+
+  // Enables line sync per elements
+  RULES.forEach((rule) => {
+    const original = md.renderer.rules[rule];
+    md.renderer.rules[rule] = (tokens, idx, options, env, self) => {
+      const token = tokens[idx];
+
+      if (token.map?.length) {
+        token.attrJoin("class", "code-line");
+        token.attrSet("data-line", token.map[0]);
+      }
+
+      const renderer = original || self.renderToken;
+      return renderer.call(self, tokens, idx, options, env, self);
+    };
+  });
+}
