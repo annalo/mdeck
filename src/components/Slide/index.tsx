8aa4524a6ccc2787b78d837c3fb17a6fa336c4c1
Anna Lo <annayylo@gmail.com>
2020-08-19 00:34:03 -0400

    rename lineNumber on slide components to be srcLine

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 interface Props {
   className: string;
+  srcLine: number;
   children: React.ReactElement;
-  lineNumber: number;
   viewBox: string;
 }
 
 export const Slide: React.FC<Props> = ({
   children,
   className,
+  srcLine,
   viewBox,
-  lineNumber,
 }: Props) => {
   return (
     <svg className={className} data-marpit-svg="" viewBox={viewBox}>
--- src/components/SlideElement/index.tsx
+++ src/components/SlideElement/index.tsx
@@ -4,14 +4,14 @@ interface Props {
   children: React.ReactElement;
   attributes: { [key: string]: string };
   elementTag: string;
-  lineNumber: number;
+  srcLine: number;
 }
 
 export function SlideElement({
   attributes,
   children,
   elementTag,
-  lineNumber,
+  srcLine,
 }: Props): React.ReactElement {
   const { class: className, ...attrs } = attributes;
   return React.createElement(elementTag, { className, ...attrs }, children);
