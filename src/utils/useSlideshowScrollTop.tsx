ed281ac59a7f9fa379d292a200d9fd356d104967
Anna Lo <annayylo@gmail.com>
2020-08-23 03:56:06 -0400

    useSlideshowScrollTop custom hook

--- src/utils/useSlideshowScrollTop.tsx
+++ src/utils/useSlideshowScrollTop.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react";
-import type { RefObject } from "react";
-import throttle from "lodash/throttle";
+import { useState, useEffect } from "react";
 
-export function useSlideshowScrollTop(ref: RefObject<HTMLDivElement>): number {
-  const [scrollTop, setScrollTop] = useState<number>(0);
+type Listener = (number) => void;
 
+const listeners: Array<Listener> = [];
+let scrollTop = 0;
+
+const setScrollTop: (number) => void = (newScrollTop: number) => {
+  scrollTop = newScrollTop;
+  listeners.forEach((listener: Listener) => {
+    listener(scrollTop);
+  });
+};
+
+export function useSlideshowScrollTop(): [number, (number) => void] {
+  const [, newListener] = useState();
   useEffect(() => {
-    const node = ref.current;
-
-    const handleScroll = throttle(() => {
-      setScrollTop(node ? node.scrollTop : 0);
-    }, 200);
-
-    node?.addEventListener("scroll", handleScroll);
-    return () => node?.removeEventListener("scroll", handleScroll);
-  }, [ref]);
-
-  return scrollTop;
+    listeners.push(newListener);
+  }, []);
+  return [scrollTop, setScrollTop];
 }
