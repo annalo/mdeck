58c867bc8432537741fb89835cd27719da0d2d20
Anna Lo <annayylo@gmail.com>
2020-08-26 01:08:42 -0400

    add overrides options to eslintrc config to override shitty ass cra

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,30 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
     "prettier",
     "prettier/react",
     "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
-  plugins: [
-    "prettier",
-    "react",
-    "react-hooks",
-    "jsx-a11y",
-    "@typescript-eslint",
+  plugins: ["react", "@typescript-eslint", "jest"],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 2020,
+    sourceType: "module",
     project: "./tsconfig.json",
   },
   rules: {
-    "import/prefer-default-export": "off",
-    "linebreak-style": "off",
-    "no-debugger": "warn",
-    "no-underscore-dangle": "off",
-    "no-unused-expressions": "off",
     "@typescript-eslint/no-unused-expressions": [
       "error",
       {
@@ -34,6 +33,11 @@ module.exports = {
         allowTernary: true,
       },
     ],
+    "import/prefer-default-export": "off",
+    "linebreak-style": "off",
+    "no-debugger": "warn",
+    "no-underscore-dangle": "off",
+    "no-unused-expressions": "off",
     "prettier/prettier": [
       "error",
       {
