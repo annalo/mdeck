663b8e7efb4eb350137a588dc85fa26ccd7bb68c
Anna Lo <annayylo@gmail.com>
2020-08-25 22:37:52 -0400

    allow ternary and short circuit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,10 @@ module.exports = {
     "linebreak-style": "off",
     "no-debugger": "warn",
     "no-underscore-dangle": "off",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      { allowShortCircuit: true, allowTernary: true },
+    ],
     "prettier/prettier": [
       "error",
       {
