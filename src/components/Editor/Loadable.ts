ed4c83e7d43b94decec11abe0f63598ff0cdc878
Anna Lo <annayylo@gmail.com>
2020-08-28 19:26:44 -0400

    reset react app bc fuck cra

--- src/components/Editor/Loadable.ts
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for Editor
- *
- */
-
-import { lazyLoad } from "utils/loadable";
-
-export const Editor = lazyLoad(() => import("./index"));
--- src/components/Preview/Loadable.ts
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for Preview
- *
- */
-
-import { lazyLoad } from "utils/loadable";
-
-export const Preview = lazyLoad(() => import("./index"));
