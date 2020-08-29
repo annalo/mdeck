4027b6d78426ad968b6c7bd2952c82dda116249b
Anna Lo <annayylo@gmail.com>
2020-08-29 19:12:10 -0400

    fix webpack aliasing for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["**/*.{js,ts,tsx}", "!**/*.d.ts"],
+  collectCoverageFrom: [
+    "**/*.{js,ts,tsx}",
+    "!**/*.d.ts",
+    "!src/*/*/Loadable.{ts,tsx}",
+  ],
+  moduleNameMapper: {
+    "^components(.*)$": "<rootDir>/src/components$1",
+    "^utils(.*)$": "<rootDir>/src/utils$1",
+  },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testRegex: ".*\\.test\\.[jt]sx?$",
 };
