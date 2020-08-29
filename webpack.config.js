d69fe9eaddd770e080654ffc76ce5d18e32d790e
Anna Lo <annayylo@gmail.com>
2020-08-29 00:34:17 -0400

    some tweaks to make HMR fastser

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = (env) => {
     entry: path.resolve(__dirname, "src", "index.tsx"),
     output: {
       path: path.resolve(__dirname, "public"),
+      pathinfo: false,
       filename: "bundle.js",
       publicPath: "/",
     },
@@ -22,7 +23,12 @@ module.exports = (env) => {
         {
           test: /\.ts?$/,
           exclude: /node_modules/,
-          use: "ts-loader",
+          use: [
+            {
+              loader: "ts-loader",
+              options: { transpileOnly: true, experimentalWatchApi: true },
+            },
+          ],
         },
         {
           test: /\.css$/i,
@@ -41,6 +47,11 @@ module.exports = (env) => {
         utils: path.resolve(__dirname, "src/utils/"),
       },
     },
+    optimization: {
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false,
+    },
     plugins: [
       new ForkTsCheckerWebpackPlugin({
         async: false,
@@ -58,6 +69,9 @@ module.exports = (env) => {
       hot: true,
       port: 3000,
       stats: "minimal",
+      watchOptions: {
+        ignored: path.resolve(__dirname, "node_modules"),
+      },
     },
     devtool: isProduction ? "source-map" : "inline-source-map",
   };
