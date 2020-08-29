4c053c6a131fece77d89382f23afb93d0439b86d
Anna Lo <annayylo@gmail.com>
2020-08-29 15:39:15 -0400

    use cross-env to sent NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,88 +2,87 @@ const path = require("path");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env) => {
-  console.log(env);
-  const isProduction = env === "production";
-  return {
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "public"),
-      pathinfo: false,
-      filename: "bundle.js",
-      publicPath: "/",
-    },
-    mode: isProduction ? "production" : "development",
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          use: "babel-loader",
-        },
-        {
-          test: /\.ts?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: { transpileOnly: true, experimentalWatchApi: true },
-            },
-          ],
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: ["file-loader"],
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      alias: {
-        components: path.resolve(__dirname, "src/components/"),
-        utils: path.resolve(__dirname, "src/utils/"),
+console.log(process.env.NODE_ENV);
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    pathinfo: false,
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  mode: isProduction ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
       },
-    },
-    optimization: {
-      removeAvailableModules: false,
-      removeEmptyChunks: false,
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-        },
-      },
-    },
-    plugins: [
-      new ForkTsCheckerWebpackPlugin({
-        async: false,
-        eslint: {
-          files: "./src/**/*.{ts,tsx,js,jsx}",
-        },
-      }),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-        typescript: {
-          diagnosticOptions: {
-            semantic: true,
-            syntactic: true,
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: { transpileOnly: true, experimentalWatchApi: true },
           },
-        },
-      }),
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
     ],
-    devServer: {
-      compress: true,
-      contentBase: path.resolve(__dirname, "public"),
-      hot: true,
-      port: 3000,
-      stats: "minimal",
-      watchOptions: {
-        ignored: /node_modules/,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      components: path.resolve(__dirname, "src/components/"),
+      utils: path.resolve(__dirname, "src/utils/"),
+    },
+  },
+  optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
       },
     },
-    devtool: isProduction ? "source-map" : "inline-cheap-source-map",
-  };
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*.{ts,tsx,js,jsx}",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
+    }),
+  ],
+  devServer: {
+    compress: true,
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true,
+    port: 3000,
+    stats: "minimal",
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+  },
+  devtool: isProduction ? "source-map" : "inline-cheap-source-map",
 };
