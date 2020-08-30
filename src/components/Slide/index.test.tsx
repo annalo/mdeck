003060345cd2ada7ce936cf29b7dcfe5b0dba66e
Anna Lo <annayylo@gmail.com>
2020-08-30 16:34:46 -0400

    test svg renders in Slide component

--- src/components/Slide/index.test.tsx
+++ src/components/Slide/index.test.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Slide } from ".";
+
+const component = (
+  <Slide className="slide" index={0} srcLine={0} viewBox="0 0 1280 720">
+    <div>Some Child Element</div>
+  </Slide>
+);
 
 describe("<Slide />", () => {
   it("should render and match the snapshot", () => {
-    const { asFragment } = render(
-      <Slide className="slide" srcLine={0} viewBox="0 0  1280 720">
-        <div>Some Child Element</div>
-      </Slide>
-    );
+    const { asFragment } = render(component);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  test("renders an svg element with viewBox and data-line attributes", () => {
+    render(component);
+
+    const svg = screen.getByRole("img");
+    screen.debug(svg);
+    expect(svg).toHaveAttribute("viewBox");
+    expect(svg).toHaveAttribute("data-line");
+  });
+
+  // test("observes itself with SlideshowObserver");
 });
