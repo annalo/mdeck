7a8e39d56a55ebff4d4a1414e118b669010bedfb
Anna Lo <annayylo@gmail.com>
2020-08-30 23:40:28 -0400

    more tests for SlideElement

--- src/components/SlideElement/index.test.tsx
+++ src/components/SlideElement/index.test.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SlideElement } from ".";
 
+const component = (
+  <SlideElement attributes={{}} elementTag="h1" srcLine={2}>
+    <div>Some Child Element</div>
+  </SlideElement>
+);
 describe("<SlideElement />", () => {
   it("should render and match the snapshot", () => {
-    const { asFragment } = render(
-      <SlideElement attributes={{}} elementTag="h1" srcLine={2}>
-        <div>Some Child Element</div>
-      </SlideElement>
-    );
+    const { asFragment } = render(component);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("renders the element type from elementTag prop", () => {
+    render(component);
+    const header = screen.getByRole("heading");
+    expect(header).toBeInTheDocument();
+  });
+
+  test("rendered element should have data-line attribute", () => {
+    const { container } = render(component);
+    expect(container.firstChild).toHaveAttribute("data-line");
   });
 });
