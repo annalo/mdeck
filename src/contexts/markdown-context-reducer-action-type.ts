c45e3e04bc397195524be9ece657f8f7fc50c557
Anna Lo <annayylo@gmail.com>
2020-09-03 02:20:21 -0400

    make markdown-context.d.ts just a ts file

--- src/contexts/markdown-context-reducer-action-type.ts
+++
@@ -1,6 +0,0 @@
-export const enum MarkdownContextReducerActionType {
-  SetHtmlString = "SET_HTML_STRING",
-  SetMd = "SET_MD",
-  SetSlideshowLineNumber = "SET_SLIDESHOW_LINE_NUMBER",
-  SetTextLineNumber = "SET_TEXT_LINE_NUMBER",
-}
