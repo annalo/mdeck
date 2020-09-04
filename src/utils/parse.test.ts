72803708aa47fcf0f51d07d79fc61db21a1684a7
Anna Lo <annayylo@gmail.com>
2020-09-04 00:10:59 -0400

    more parse test

--- src/utils/parse.test.ts
+++ src/utils/parse.test.ts
@@ -1,7 +1,7 @@
 import { parse } from "./parse";
 
 describe("parse/1", () => {
-  test("should parse markdown string into html string", () => {
+test("should parse markdown string into html string", () => {
     const markdown = "# Slide 1\n---\n## Slide 2";
     const expectedHtmlString = `<div class="marpit"><svg data-marpit-svg="" viewBox="0 0 1280 720" data-line="0"><foreignObject width="1280" height="720"><section id="1">
 <h1 data-line="0">Slide 1</h1>
@@ -12,5 +12,11 @@ describe("parse/1", () => {
 </foreignObject></svg></div>`;
 
     expect(parse(markdown)).toBe(expectedHtmlString);
+  });
+
+test("should inject line number as 'data-line' attribute", () => {
+    const markdown = "# Slide 1\n---\n## Slide 2";
+    const htmlString = parse(markdown)
+    expect(htmlString.match(/data-line/g)).toHaveLength(4)
   });
 });
