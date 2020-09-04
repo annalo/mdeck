581fe282e3a6e1be5566cf1ae258abf6fd36017f
Anna Lo <annayylo@gmail.com>
2020-09-04 00:26:01 -0400

    add note for web-worker.d.ts declaration

--- src/types/web-worker.d.ts
+++ src/types/web-worker.d.ts
@@ -1,3 +1,9 @@
+/*
+ *
+ * A custom module for worker exports is required to integrated with Typescript:
+ * https://webpack.js.org/loaders/worker-loader/#integrating-with-typescript
+ *
+ */
 declare module "worker-loader!*" {
   class WebpackWorker extends Worker {
     constructor();
