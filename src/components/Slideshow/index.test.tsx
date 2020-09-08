c59fe1f09b97decce63e1217d8534efbf92a0efd
Anna Lo <annayylo@gmail.com>
2020-09-08 16:23:25 -0400

    add test for useTrackSlideshowScroll

--- src/components/Slideshow/index.test.tsx
+++ src/components/Slideshow/index.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { useContext } from "react";
+import { fireEvent, render } from "@testing-library/react";
 import { act, renderHook } from "@testing-library/react-hooks";
 import scrollIntoView from "smooth-scroll-into-view-if-needed";
 
@@ -7,9 +7,15 @@ import {
   CODE_LINE_CLASS_NAME,
   DATA_LINE_ATTRIBUTE,
 } from "utils/parsePlugins/injectLineNumber";
+import {
+  MarkdownContext,
+  MarkdownContextProvider,
+} from "contexts/MarkdownContext";
+
 import { Slideshow } from ".";
 import { useObservable } from "./useObservable";
 import { useSyncSlideshow } from "./useSyncSlideshow";
+import { useTrackSlideshowScroll } from "./useTrackSlideshowScroll";
 
 jest.mock("smooth-scroll-into-view-if-needed");
 afterEach(() => jest.clearAllMocks());
@@ -43,7 +49,7 @@ describe("<Slideshow />", () => {
     test("should sync slideshow to textLineNumber if there's an element with the data-line", () => {
       const textLineNumber = 2;
       const element = { test: "test" };
-      const entries = { 2: element };
+      const entries = { [textLineNumber]: element };
 
       renderHook(() => useSyncSlideshow({ entries, textLineNumber }));
 
@@ -58,6 +64,58 @@ describe("<Slideshow />", () => {
       renderHook(() => useSyncSlideshow({ entries, textLineNumber }));
 
       expect(scrollIntoView).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("useTrackSlideshowScroll", () => {
+    const mockBoundingClientRect = (top) =>
+      jest.fn(() => {
+        return {
+          width: 120,
+          height: 120,
+          top,
+          left: 0,
+          x: 0,
+          y: 1,
+          bottom: 0,
+          right: 0,
+          toJSON: jest.fn(),
+        };
+      });
+
+    test("should set slideshowLineNumber to the top most element's data-line", () => {
+      const slideshow = document.createElement("div");
+      const h1 = document.createElement("h1");
+      const h2 = document.createElement("h2");
+      const h3 = document.createElement("h3");
+
+      h1.getBoundingClientRect = mockBoundingClientRect(-5);
+      h2.getBoundingClientRect = mockBoundingClientRect(2);
+      h3.getBoundingClientRect = mockBoundingClientRect(50);
+
+      const lineNumber = 2;
+      const entries = { 0: h1, 50: h3, [lineNumber]: h2 };
+      const ref = { current: slideshow };
+
+      const wrapper = ({ children }) => (
+        <MarkdownContextProvider>{children}</MarkdownContextProvider>
+      );
+      const { result } = renderHook(
+        () => {
+          const { state, dispatch } = useContext(MarkdownContext);
+          useTrackSlideshowScroll({ dispatch, entries, isActive: true, ref });
+          return state;
+        },
+        { wrapper }
+      );
+
+      expect(result.current.slideshowLineNumber).toBe(0);
+
+      act(() => {
+        fireEvent.scroll(slideshow, { target: { scrollY: 100 } });
+      });
+
+      expect(result.current.slideshowLineNumber).toBe(lineNumber);
     });
   });
 });
