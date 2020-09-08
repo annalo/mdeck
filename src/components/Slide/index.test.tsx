4a19c9eedf6a7f25a7d136cc298c6625598672d1
Anna Lo <annayylo@gmail.com>
2020-09-08 00:17:43 -0400

    add test for observing Slide with Slideshow observer

--- src/components/Slide/index.test.tsx
+++ src/components/Slide/index.test.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 
-import { debug } from "webpack";
+import {
+  CODE_LINE_CLASS_NAME,
+  DATA_LINE_ATTRIBUTE,
+} from "utils/parsePlugins/injectLineNumber";
+
+import { useObservable } from "components/Slideshow/useObservable";
 import { Slide } from ".";
+import { useObserve } from "./useObserve";
 
 const observeMock = jest.fn();
 const component = (
@@ -33,5 +40,25 @@ describe("<Slide />", () => {
         </svg>
       </div>
     `);
+  });
+
+  test("should observe itself", () => {
+    /* Create element with data-line for observer to pick up */
+    const div = document.createElement("div");
+    const node = document.createElement("h1");
+    node.className = CODE_LINE_CLASS_NAME;
+    node.setAttribute(DATA_LINE_ATTRIBUTE, "0");
+    div.appendChild(node);
+
+    const elements = <h1>Title</h1>;
+    const ref = { current: div };
+
+    const { result } = renderHook(() => {
+      const { entries, observe } = useObservable();
+      useObserve({ elements, ref, observe });
+      return entries;
+    });
+
+    expect(result.current).toMatchObject({ 0: node });
   });
 });
