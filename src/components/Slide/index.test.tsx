fff544bb7aafd234f08f8d5e3cd78bb8e2b9eeef
Anna Lo <annayylo@gmail.com>
2020-09-08 00:00:26 -0400

    Slide tests

--- src/components/Slide/index.test.tsx
+++ src/components/Slide/index.test.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
+import { debug } from "webpack";
 import { Slide } from ".";
 
 const observeMock = jest.fn();
 const component = (
-  <Slide htmlString="<h1>Title</h1>" index={0} observe={observeMock}>
-    <div>Some Child Element</div>
-  </Slide>
+  <Slide
+    htmlString="<svg><h1>Title</h1></svg>"
+    index={0}
+    observe={observeMock}
+  />
 );
 
 describe("<Slide />", () => {
   test("should render and match the snapshot", () => {
     const { asFragment } = render(component);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("should render htmlString as React elements", () => {
+    const { container } = render(component);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        id="slide-1"
+      >
+        <svg>
+          <h1>
+            Title
+          </h1>
+        </svg>
+      </div>
+    `);
   });
 });
