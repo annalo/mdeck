0952da31f248499c2d42b52a5e31747afbd2fec7
Anna Lo <annayylo@gmail.com>
2020-09-10 01:50:46 -0400

    render all slides in parse test and not just htmlArray[0]

--- src/utils/parse.test.tsx
+++ src/utils/parse.test.tsx
@@ -3,13 +3,16 @@ import { render } from "@testing-library/react";
 import { parse } from "./parse";
 
 describe("parse/1", () => {
+  const slides = (htmlArray) =>
+    htmlArray.map((html, i) => (
+      <div dangerouslySetInnerHTML={{ __html: html }} key={`slide-${i + 1}`} />
+    ));
+
   test("should parse markdown string into html string", () => {
     const markdown = "# Slide 1\n---\n## Slide 2\n* Bullet 1\n* Bullet 2";
     const htmlArray = parse(markdown);
 
-    const { asFragment } = render(
-      <div dangerouslySetInnerHTML={{ __html: htmlArray[0] }} />
-    );
+    const { asFragment } = render(slides(htmlArray));
     expect(asFragment()).toMatchSnapshot();
   });
 
@@ -25,9 +28,7 @@ describe("parse/1", () => {
       "```js\nvar foo = function (bar) {\n  return bar++;\n};\n\nconsole.log(foo(5));\n```";
     const htmlArray = parse(markdown);
 
-    const { asFragment } = render(
-      <div dangerouslySetInnerHTML={{ __html: htmlArray[0] }} />
-    );
+    const { asFragment } = render(slides(htmlArray));
     expect(asFragment()).toMatchSnapshot();
   });
 });
