cc8a2464782c8df14e94aa34f3fe234959b6dac9
Anna Lo <annayylo@gmail.com>
2020-09-12 00:38:30 -0400

    Slide useCodeLineObserve

---
+++ src/components/Slide/useCodeLineObserve.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import type { RefObject } from "react";
+
+import { useCodeLineObserver } from "contexts/CodeLineObserver";
+import {
+  CODE_LINE_CLASS_NAME,
+  DATA_LINE_ATTRIBUTE,
+} from "utils/parsePlugins/injectLineNumber";
+
+function useCodeLineObserve(ref: RefObject<Element>): void {
+  const { observe } = useCodeLineObserver();
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      const entries = {};
+      const codeLineElements: NodeListOf<Element> = node?.querySelectorAll(
+        `.${CODE_LINE_CLASS_NAME}`
+      );
+
+      codeLineElements.forEach((element) => {
+        const dataLine = element.getAttribute(DATA_LINE_ATTRIBUTE);
+        if (dataLine) entries[dataLine] = element;
+      });
+
+      observe(entries);
+    }
+  }, [ref, observe]);
+}
+
+export { useCodeLineObserve };
