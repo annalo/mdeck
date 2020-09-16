eb0579efea7bb7ebc4d1f4349a54caef18961145
Anna Lo <annayylo@gmail.com>
2020-09-15 22:09:59 -0400

    useNavigation to usePresentation

--- src/components/Preview/usePresentation.ts
+++ src/components/Preview/usePresentation.ts
@@ -4,62 +4,49 @@ import scrollIntoView from "smooth-scroll-into-view-if-needed";
 
 import { useSlideEntries } from "contexts/SlideObserver";
 
-function useNavigation(): () => void {
+function usePresentation(): () => void {
   const [presentationMode, togglePresentation] = useState(false);
-  const [slideNumber, setSlideNumber] = useState<SlideNumber | null>(null);
+  const [slideNumber, setSlideNumber] = useState<SlideNumber>(1);
 
   const slideEntries = useSlideEntries();
   const slideCount = Object.keys(slideEntries).length;
 
   const requestPresentation = useCallback(() => {
     const slideshow = document.querySelector("#slideshow");
-    if (slideshow)
-      (screenfull as Screenfull).request(slideshow).then(() => {
-        console.log("request fullscreen");
-        togglePresentation(true);
-      });
+    if (slideshow) (screenfull as Screenfull).request(slideshow);
   }, []);
 
   useEffect(() => {
-    const fullScreen = screenfull as Screenfull;
-    const exitFullscreen = () => {
-      if (!fullScreen.isFullscreen) togglePresentation(false);
+    const fullScreen = screenfull as Screenfull; // for Typescript
+    const setPresentationMode = () => {
+      togglePresentation(fullScreen.isFullscreen);
+      if (fullScreen.isFullscreen) setSlideNumber(1);
     };
-    fullScreen.on("change", exitFullscreen);
-    return () => fullScreen.off("change", exitFullscreen);
-  }, [togglePresentation]);
+
+    fullScreen.on("change", setPresentationMode);
+    return () => fullScreen.off("change", setPresentationMode);
+  }, []);
 
   useEffect(() => {
-    console.log("use effect set slide number 1 when presentationMode");
-    presentationMode ? setSlideNumber(1) : setSlideNumber(null);
-  }, [presentationMode]);
-
-  useEffect(() => {
-    console.log("useeffect slidenumber changed", slideNumber);
-    if (slideNumber) {
-      const slide = slideEntries[slideNumber];
-      scrollIntoView(slide);
-    }
-  }, [slideNumber, slideEntries]);
+    if (presentationMode) scrollIntoView(slideEntries[slideNumber]);
+  }, [presentationMode, slideNumber, slideEntries]);
 
   const nextSlide = useCallback(
     () =>
       setSlideNumber((current) =>
-        current && current < slideCount ? current + 1 : current
+        current < slideCount ? current + 1 : current
       ),
     [slideCount]
   );
   const previousSlide = useCallback(
-    () =>
-      setSlideNumber((current) =>
-        current && current > 1 ? current - 1 : current
-      ),
+    () => setSlideNumber((current) => (current > 1 ? current - 1 : current)),
     []
   );
 
   useEffect(() => {
     const handleKeyDown = (e) => {
-      console.log("handlekeydown");
+      e.preventDefault();
+
       switch (e.keyCode) {
         case 37: // ArrowLeft
         case 38: // ArrowUp
@@ -87,4 +74,4 @@ function useNavigation(): () => void {
   return requestPresentation;
 }
 
-export { useNavigation };
+export { usePresentation };
