5a3762f1ff4aee42e9fae5d50cabe17aaa077261
Anna Lo <annayylo@gmail.com>
2020-09-16 02:15:43 -0400

    add some presenter tests

--- src/components/Preview/index.test.tsx
+++ src/components/Preview/index.test.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 
 import {
+  MARKDOWN_CONTEXT_DEFAULT_INITIAL_STATE,
   MarkdownProvider,
   useMarkdownDispatch,
 } from "contexts/MarkdownContext";
@@ -63,6 +64,38 @@ describe("<Preview />", () => {
 
       const workerInstance = MarkdownWorker.mock.instances[0];
       expect(workerInstance.parse).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe("usePresentation", () => {
+    const htmlArray = [
+      "<svg data-line='1'>1</svg>",
+      "<svg data-line='2'>2</svg>",
+      "<svg data-line='3'>3</svg>",
+    ];
+    const wrapperWithMarkdownValues = ({ children }) => (
+      <MarkdownProvider
+        initialValues={{ ...MARKDOWN_CONTEXT_DEFAULT_INITIAL_STATE, htmlArray }}
+      >
+        <SlideObserverProvider>
+          <CodeLineObserverProvider>{children}</CodeLineObserverProvider>
+        </SlideObserverProvider>
+      </MarkdownProvider>
+    );
+
+    test("should not be in fullscreen by default", () => {
+      render(<Preview />, { wrapper: wrapperWithMarkdownValues });
+      expect(document.fullscreen).not.toBeTruthy();
+    });
+
+    test("should fullscreen slideshow when fullscreen button is clicked", async () => {
+      render(<Preview />, { wrapper: wrapperWithMarkdownValues });
+
+      expect(document.fullscreen).not.toBeTruthy();
+
+      const button = screen.getByRole("button");
+      fireEvent.click(button);
+      waitFor(() => expect(document.fullscreen).toBeTruthy());
     });
   });
 });
