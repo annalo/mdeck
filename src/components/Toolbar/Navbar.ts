aa9cfb97b61d34ced82185a75628c6c41c9777ef
Anna Lo <annayylo@gmail.com>
2020-09-18 04:54:24 -0400

    Navbar styling

--- src/components/Toolbar/Navbar.ts
+++ src/components/Toolbar/Navbar.ts
@@ -9,9 +9,9 @@ const Navbar = styled.nav`
   }
 
   background-color: #ededed;
-  -webkit-box-shadow: 0px 1px 2px #e0e0e0;
-  -moz-box-shadow: 0px 1px 2px #e0e0e0;
-  box-shadow: 0px 1px 2px #e0e0e0;
+  -webkit-box-shadow: 0px 0px 1.5px #2f2f2f;
+  -moz-box-shadow: 0px 0px 1.5px #2f2f2f;
+  box-shadow: 0px 0px 1.5px #2f2f2f;
 
   height: ${NAVBAR_HEIGHT}px;
   overflow: hidden;
