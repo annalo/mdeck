6fea546e1ea8f14d74e2636d79ad6a337cc3dd3b
Anna Lo <annayylo@gmail.com>
2020-09-19 01:28:13 -0400

    use ThemeProvider props to use navbarheightin Editor styling

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -5,7 +5,6 @@ import { MarkdownProvider } from "contexts/MarkdownContext";
 import { SlideObserverProvider } from "contexts/SlideObserver";
 import { CodeLineObserverProvider } from "contexts/CodeLineObserver";
 
-import { NAVBAR_HEIGHT } from "components/Toolbar/Navbar";
 import { Toolbar } from "components/Toolbar/Loadable";
 import { TextEditor } from "components/TextEditor/Loadable";
 import { Preview } from "components/Preview/Loadable";
@@ -13,7 +12,7 @@ import { Preview } from "components/Preview/Loadable";
 const Container = styled.div`
   display: flex;
   height: 100%;
-  padding-bottom: ${NAVBAR_HEIGHT + 2}px;
+  padding-bottom: ${(props) => props.theme.navBarHeight + 2}px;
 `;
 
 const Editor = memo(function Editor() {
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 import { Editor } from "components/Editor/Loadable";
+import { ThemeProvider } from "./ThemeProvider";
 
 const Div = styled.div`
   height: 100%;
 `;
 
 const Home: React.FC = () => (
-  <Div id="main">
-    <Editor />
-  </Div>
+  <ThemeProvider>
+    <Div id="main">
+      <Editor />
+    </Div>
+  </ThemeProvider>
 );
 
 export { Home };
