caccac077e9f15102ab0a0b7dda9d59dada86ea1
Anna Lo <annayylo@gmail.com>
2020-09-22 15:07:33 -0400

    styled component theme provider

--- src/components/Home/ThemeProvider.tsx
+++ src/components/Home/ThemeProvider.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
+// light to dark
+const colorScheme = {
+  snow: "#fafafa",
+  lightgrey: "#d4d4d4",
+  silver: "#bababa",
+  grey: "#888888",
+  nightrider: "#2f2f2f",
+};
+
 const theme = {
+  colorScheme,
   navBarHeight: 25,
-  textAreaBackgroundColor: "#fafafa",
-  textColor: "#2f2f2f",
+  textAreaBackgroundColor: colorScheme.snow,
+  textColor: colorScheme.nightrider,
 };
 
 const ThemeProvider = ({
