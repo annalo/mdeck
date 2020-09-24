ce29a21c4ca63b3a9b3eef312b0e5101fab365c1
Anna Lo <annayylo@gmail.com>
2020-09-23 21:10:55 -0400

    read selected file in LoadFile

--- src/components/Toolbar/LoadFile/index.tsx
+++ src/components/Toolbar/LoadFile/index.tsx
@@ -10,10 +10,36 @@ const FileLoader = styled.input`
   display: none;
 `;
 
+const readUploadedFileAsText = (inputFile) => {
+  const temporaryFileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    temporaryFileReader.onerror = () => {
+      temporaryFileReader.abort();
+      reject(new DOMException("Problem parsing input file."));
+    };
+
+    temporaryFileReader.onload = () => {
+      resolve(temporaryFileReader.result);
+    };
+    temporaryFileReader.readAsText(inputFile);
+  });
+};
+
 const LoadFileMenuItem = (): React.ReactElement => {
   const fileLoaderRef = useRef<HTMLInputElement>(null);
 
   const handleClick = () => fileLoaderRef?.current?.click();
+  const handleLoad = async (e) => {
+    const file = e.target.files[0];
+
+    try {
+      const fileContents = await readUploadedFileAsText(file);
+      console.log(fileContents);
+    } catch (error) {
+      console.warn(error.message);
+    }
+  };
 
   return (
     <>
@@ -21,6 +47,7 @@ const LoadFileMenuItem = (): React.ReactElement => {
         ref={fileLoaderRef}
         accept=".md,.txt"
         id="fileLoader"
+        onChange={handleLoad}
         title="Load Markdown File"
         type="file"
       />
