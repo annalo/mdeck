2a55bfa46cf0db2a00ebd9773a678700a752264e
Anna Lo <annayylo@gmail.com>
2020-09-24 20:22:44 -0400

    render MenuItem instead of MenuItemWithTooltip when SaveFileForm

--- src/components/Toolbar/SaveFile/index.tsx
+++ src/components/Toolbar/SaveFile/index.tsx
@@ -4,7 +4,7 @@ import { saveAs } from "file-saver";
 import { useMarkdownState } from "contexts/MarkdownContext";
 
 import SaveIcon from "icons/save.svg";
-import { MenuItemWithTooltip } from "../MenuItem";
+import { MenuItem, MenuItemWithTooltip } from "../MenuItem";
 import { Tooltip } from "../Tooltip";
 import { SaveFileForm } from "./SaveFileForm";
 
@@ -19,26 +19,24 @@ const SaveFileMenuItem = (): React.ReactElement => {
 
     const text = ev.target.filename.value;
     const blob = new Blob([md], { type: "text/markdown" });
-    // TODO chain in promises
+
     saveAs(blob, text);
     setFilename(text);
     toggleSave(false);
   };
 
-  return (
+  return save ? (
+    <MenuItem>
+      <SaveFileForm
+        filename={filename}
+        saveFile={saveFile}
+        toggleSave={toggleSave}
+      />
+    </MenuItem>
+  ) : (
     <MenuItemWithTooltip onClick={() => toggleSave(true)}>
-      {save ? (
-        <SaveFileForm
-          filename={filename}
-          saveFile={saveFile}
-          toggleSave={toggleSave}
-        />
-      ) : (
-        <>
-          <SaveIcon />
-          <Tooltip text="SAVE" />
-        </>
-      )}
+      <SaveIcon />
+      <Tooltip text="SAVE" />
     </MenuItemWithTooltip>
   );
 };
