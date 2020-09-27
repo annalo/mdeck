4ec9c9f1ea3ec21ba9fc284caf5c8b4115fe78c6
Anna Lo <annayylo@gmail.com>
2020-09-27 18:26:46 -0400

    PresentationMode toolbar onclick present presentation

--- src/components/Toolbar/PresentationMode.tsx
+++ src/components/Toolbar/PresentationMode.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
+import { usePresentationActions } from "contexts/PresentationContext";
 import PresentationIcon from "icons/video-camera.svg";
 import { MenuItemRight, iconAnimation } from "./MenuItem";
 
@@ -8,10 +9,13 @@ const Icon = styled(PresentationIcon)`
   ${iconAnimation}
 `;
 
-const PresentationMode: React.FC = () => (
-  <MenuItemRight>
-    <Icon />
-  </MenuItemRight>
-);
+const PresentationMode: React.FC = () => {
+  const { present } = usePresentationActions();
+  return (
+    <MenuItemRight onClick={present} role="button">
+      <Icon />
+    </MenuItemRight>
+  );
+};
 
 export { PresentationMode };
