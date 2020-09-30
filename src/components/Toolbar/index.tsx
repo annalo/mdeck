77b743e6c08e2dc9340515f9aade1b8871dace7c
Anna Lo <annayylo@gmail.com>
2020-09-29 23:55:57 -0400

    disable fullscreen button if no md

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useMarkdownState } from "contexts/MarkdownContext";
+
 import PresentationSvg from "icons/video-camera.svg";
 
 import { Header } from "./Header";
@@ -14,24 +16,27 @@ interface ToolbarProps {
   requestPresentation: RequestPresentation;
 }
 
-const Toolbar: React.FC<ToolbarProps> = ({ requestPresentation }) => (
-  <Header>
-    <Menu>
-      <LoadFileMenuItem />
-      <SaveFileMenuItem />
+const Toolbar: React.FC<ToolbarProps> = ({ requestPresentation }) => {
+  const { md } = useMarkdownState();
+  return (
+    <Header>
+      <Menu>
+        <LoadFileMenuItem />
+        <SaveFileMenuItem />
 
-      <MenuItemRight
-        disabled
-        aria-label="request-presentation"
-        onClick={requestPresentation}
-        role="button"
-      >
-        <Icon>
-          <PresentationSvg />
-        </Icon>
-      </MenuItemRight>
-    </Menu>
-  </Header>
-);
+        <MenuItemRight
+          aria-label="request-presentation"
+          disabled={md === ""}
+          onClick={requestPresentation}
+          role="button"
+        >
+          <Icon>
+            <PresentationSvg />
+          </Icon>
+        </MenuItemRight>
+      </Menu>
+    </Header>
+  );
+};
 
 export { Toolbar };
